0f9bc6c1f23f5ac74b354e406a2d78fe27d784d8
Nebojša Bogosavljev <nebojsa253@gmail.com>
2022-02-22 22:59:11 +0100

    added ann

---
+++ SoftProject/ann.py
@@ -0,0 +1,190 @@
+import os
+import pathlib
+import pickle
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from PIL import Image, ImageOps
+from sklearn.utils import shuffle
+from matplotlib import rcParams
+import warnings
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+warnings.filterwarnings('ignore')
+rcParams['figure.figsize'] = (18, 8)
+rcParams['axes.spines.top'] = False
+rcParams['axes.spines.right'] = False
+
+
+def process_image(img_path: str) -> np.array:
+    img = Image.open(img_path)
+    img = ImageOps.grayscale(img)
+    img = img.resize(size=(180, 180))
+    img = np.ravel(img) / 255.0
+    return img
+
+
+def process_folder(folder: pathlib.PosixPath) -> pd.DataFrame:
+    # We'll store the images here
+    processed = []
+
+    # For every image in the directory
+    for img in folder.iterdir():
+        # Ensure JPG
+        if img.suffix == '.jpeg':
+            # Two images failed for whatever reason, so let's just ignore them
+            try:
+                processed.append(process_image(img_path=str(img)))
+            except Exception as _:
+                continue
+
+    # Convert to pd.DataFrame
+    processed = pd.DataFrame(processed)
+    # Add a class column - dog or a cat
+    processed['class'] = folder.parts[-1]
+
+    return processed
+
+def defineTrainingImages():
+
+    # Training set
+    train_covid = process_folder(folder=pathlib.Path.cwd().joinpath('train/covid'))
+    train_normal = process_folder(folder=pathlib.Path.cwd().joinpath('train/normal'))
+    train_set = pd.concat([train_covid, train_normal], axis=0)
+    with open('train_set.pkl', 'wb') as f:
+        pickle.dump(train_set, f)
+    # Test set
+    test_covid = process_folder(folder=pathlib.Path.cwd().joinpath('test/covid'))
+    test_normal = process_folder(folder=pathlib.Path.cwd().joinpath('test/normal'))
+    test_set = pd.concat([test_covid, test_normal], axis=0)
+    with open('test_set.pkl', 'wb') as f:
+        pickle.dump(test_set, f)
+
+    # Validation set
+    valid_covid = process_folder(folder=pathlib.Path.cwd().joinpath('val/covid'))
+    valid_normal = process_folder(folder=pathlib.Path.cwd().joinpath('val/normal'))
+    valid_set = pd.concat([valid_covid, valid_normal], axis=0)
+
+    with open('valid_set.pkl', 'wb') as f:
+        pickle.dump(valid_set, f)
+
+    train_set.head()
+
+    train_set = shuffle(train_set).reset_index(drop=True)
+    valid_set = shuffle(valid_set).reset_index(drop=True)
+
+    X_train = train_set.drop('class', axis=1)
+    y_train = train_set['class']
+
+    X_valid = valid_set.drop('class', axis=1)
+    y_valid = valid_set['class']
+
+    X_test = test_set.drop('class', axis=1)
+    y_test = test_set['class']
+    y_train.factorize()
+
+    y_train = tf.keras.utils.to_categorical(y_train.factorize()[0], num_classes=2)
+    y_valid = tf.keras.utils.to_categorical(y_valid.factorize()[0], num_classes=2)
+    y_test = tf.keras.utils.to_categorical(y_test.factorize()[0], num_classes=2)
+    class_names = ['covid', 'normal']
+    print(class_names)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(2048, activation='relu'),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(2, activation='softmax'),
+    ])
+
+    model.compile(
+        loss=tf.keras.losses.categorical_crossentropy,
+        optimizer=tf.keras.optimizers.Adam(),
+        metrics=['accuracy']
+    )
+
+    history = model.fit(
+
+        X_train,
+        y_train,
+        epochs=10,
+        batch_size=128,
+        validation_data=(X_valid, y_valid)
+    )
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(10)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+    probability_model = tf.keras.Sequential([model,
+                                             tf.keras.layers.Softmax()])
+
+    predictions = probability_model.predict(X_test, batch_size= 128,verbose = 0)
+    for i in range(len(X_test)):
+        print(
+            "{}. image most likely belongs to {} with a {:.5f} percent confidence."
+
+                .format(i + 1, class_names[np.argmax(predictions[i])], 100 * np.max(predictions[i]))
+        )
+
+    num_covid, num_correct_covid = 0, 0
+    num_normal, num_correct_normal = 0, 0
+    num_correct_normal2, num_correct_covid2 = 0, 0
+    for img_path in pathlib.Path.cwd().joinpath('test/covid').iterdir():
+        try:
+            img = process_image(img_path=str(img_path))
+            pred = model.predict(tf.expand_dims(img, axis=0))
+            pred = pred.argmax()
+            num_covid += 1
+            if pred == 0:
+                num_correct_covid += 1
+            else:
+                num_correct_normal += 1
+
+        except Exception as e:
+            continue
+    print("Amount of patients with covid:", num_covid)
+    print("Amount of wrong covid classification:", num_correct_normal)
+    print("Amount of correct covid classification:", num_correct_covid)
+    print("Accuracy:", num_correct_covid / num_covid)
+    for img_path in pathlib.Path.cwd().joinpath('test/normal').iterdir():
+        try:
+            img = process_image(img_path=str(img_path))
+            pred = model.predict(tf.expand_dims(img, axis=0))
+            pred = pred.argmax()
+            num_normal += 1
+            if pred == 1:
+                num_correct_normal2 += 1
+            else:
+                num_correct_covid2 += 1
+        except Exception as e:
+            continue
+    print("Amount of patients with normal lungs:", num_normal)
+    print("Amount of correct normal predictions:", num_correct_normal2)
+    print("Amount of wrong normal predictions:", num_correct_covid2)
+    print("Accuracy:", num_correct_normal2 / num_normal)
+
+
+defineTrainingImages()
--- SoftProject/main.py
+++ SoftProject/main.py
@@ -8,14 +8,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import PIL
-
+from PIL import Image
+import pathlib
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-
-
+from keras.preprocessing.image import ImageDataGenerator
 def load_image(path):
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)
+
+
+def prepare_single_image(img_path: str) -> np.array:
+    img = Image.open(img_path)
+    img = img.resize(size=(180, 180))
+    return np.array(img) / 255.0
 
 
 def display_image(image):
@@ -43,23 +49,20 @@ def loadimage(train_dir):
 
 def defineTrainingImages(pathFolder):
     data_dir = 'train'
-    batch_size = 32
-    img_height = 180
-    img_width = 180
+    validation_data = 'val'
+    batch_size = 64
+    img_height = 150
+    img_width = 150
 
     train_ds = tf.keras.utils.image_dataset_from_directory(
         data_dir,
-        validation_split=0.2,
-        subset="training",
-        seed=123,
+        seed=42,
         image_size=(img_height, img_width),
         batch_size=batch_size)
 
     val_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
+        validation_data,
+        seed=42,
         image_size=(img_height, img_width),
         batch_size=batch_size)
 
@@ -79,26 +82,26 @@ def defineTrainingImages(pathFolder):
     train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
     val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-    normalization_layer = layers.Rescaling(1. / 255)
-
-    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-    image_batch, labels_batch = next(iter(normalized_ds))
-    first_image = image_batch[0]
-    # Notice the pixel values are now in `[0,1]`.
-    print(np.min(first_image), np.max(first_image))
-
     num_classes = len(class_names)
+
+    data_augmentation = tf.keras.Sequential([
+        layers.RandomFlip("horizontal_and_vertical"),
+        layers.RandomRotation(0.2),
+        layers.RandomTranslation(height_factor=0.2, width_factor = 0.2)
+    ])
 
     model = Sequential([
         layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
+        data_augmentation,
         layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(128, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(256, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
         layers.Dense(num_classes)
     ])
 
@@ -108,7 +111,7 @@ def defineTrainingImages(pathFolder):
 
     model.summary()
 
-    epochs = 5
+    epochs = 20
     history = model.fit(
         train_ds,
         validation_data=val_ds,
@@ -137,24 +140,39 @@ def defineTrainingImages(pathFolder):
     plt.title('Training and Validation Loss')
     plt.show()
 
+    #train_datagen_augmented = ImageDataGenerator
+
+    num_total_cat, num_correct_cat = 0, 0
+    num_total_dog, num_correct_dog = 0, 0
+
+
+
     for directory, subdirectories, files in os.walk(pathFolder):
         for file in files:
             path = os.path.join(directory, file)
             img = tf.keras.utils.load_img(
                 path, target_size=(img_height, img_width)
             )
-
+            i += 1
             img_array = tf.keras.utils.img_to_array(img)
-            img_array = tf.expand_dims(img_array, 0)  # Create a batch
+            img_array = tf.expand_dims(img_array, 0)
 
             predictions = model.predict(img_array)
-
+            pred = predictions.argmax()
+            if pred == 1:
+                num_correct_dog += 1
+            else:
+                num_total_cat += 1
             score = tf.nn.softmax(predictions[0])
 
+
             print(
-                "This image most likely belongs to {} with a {:.2f} percent confidence."
-                    .format(class_names[np.argmax(score)], 100 * np.max(score))
+                "{}. image most likely belongs to {} with a {:.2f} percent confidence."
+                    .format(i,class_names[np.argmax(score)], 100 * np.max(score))
             )
+
+        print(num_correct_dog)
+        print(num_correct_cat)
 
 
 def loadnormal():
@@ -167,5 +185,5 @@ def loadcovid():
     defineTrainingImages(pathFolder)
 
 
-loadnormal()
 loadcovid()
+loadnormal()
